742ec4f2500b9cdeeb92714e9aca2f97db3aee1c
Emelie Sidesiö <esidesio@gmail.com>
2020-10-15 13:29:49 +0200

    Add darkmode functionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { DarkToggle } from "./DarkToggle"
 
 export default function Header() {
   return (
     <div className="header">
+      <DarkToggle />
       <ul className="navigation">
         <li>
           <a className="link" href="#resume">
